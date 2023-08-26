adf283974f56342188a3d6468ec695c3496fdb0b
Kirty Kumar Verma <32432876+Kirtykumarverma@users.noreply.github.com>
2023-08-26 14:33:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId= 65778
+var accountPassword = "hitesh"
+let accountEmail = "hitesh@email.com" // DONT USE VAR BECAUSE OF BLOCK  
+accountCity = "jaipur"
+let accountState;
+
+accountPassword = "Happt"
+accountEmail="happy@gmial.com"
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+console.log(accountId);
